18c6d8e56902217c1d474a83c8f68b2aefd5d129
Aditya Basu <mitthu@google.com>
2019-07-12 19:12:14 -0400

    get non IOMMU version working

--- main.c
+++ main.c
@@ -2,30 +2,6 @@
 UserSpace CBDMA Driver
 ======================
 
-TODO
-----
-- port struct desc + status combo from kernel
-- malloc src and dst regions for performing memcopy
-- construct the struct on the page
-- open #cbdma/ucopy and write the addr of desc
-- print out the buffers & status field after return
-- add struct ucdma contains: struct desc, status, dmacount
-
-TODO: in kernel/../cbdm.c
--------------------------
-- create #cbdma/ucopy file
-- write function: issue_dma()
-    * takes a struct ucdma
-    * issue the relevant writes and update struct ucdma
-- on open: construct help buffer
-- on read: print help
-- on write: (variant #1)
-    * convert addr to kaddr
-    * read the struct and convert all user-space addrs to paddr variants
-    * call issue_dma() and return (blocking call)
-- on write: (variant #2)
-    * this will be the IOMMU enabled version
-
 For kernel space
 ----------------
 uintptr_t uva2kva(struct proc *p, void *uva, size_t len, int prot)
@@ -37,9 +13,34 @@ returns a KVA, which you can convert to a phys addr with PADDR().
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <string.h>
+#include <inttypes.h>
+#include <fcntl.h>
 
 /* Get the 2^20th page number. 2^20 * 4k = 4GB */
 #define PAGENUM (1 << 20)
+#define BUFFERSIZE 20
+
+/* Descriptor structue as defined in the programmer's guide.
+ * It describes a single DMA transfer
+ */
+struct desc {
+        uint32_t  xfer_size;
+        uint32_t  descriptor_control;
+        uint64_t  src_addr;
+        uint64_t  dest_addr;
+        uint64_t  next_desc_addr;
+        uint64_t  next_source_address;
+        uint64_t  next_destination_address;
+        uint64_t  reserved0;
+        uint64_t  reserved1;
+} __attribute__((packed));
+
+/* describe a DMA */
+struct ucbdma {
+        struct desc             desc;
+        volatile uint64_t       status;
+        uint16_t                ndesc;
+};
 
 void panic(char *str) {
 	perror(str);
@@ -51,7 +52,7 @@ void *map_page()
 	void* region;
 	size_t pagesize = getpagesize();
 
-	printf("System page size: %zu bytes\n", pagesize);
+	printf("[user] page size: %zu bytes\n", pagesize);
 	
 	region = mmap((void*) (pagesize * PAGENUM), pagesize,
 		PROT_READ|PROT_WRITE|PROT_EXEC,MAP_ANON|MAP_PRIVATE, 0, 0);
@@ -60,6 +61,12 @@ void *map_page()
 		panic("cannot mmap");
 
 	return region;
+}
+
+void test_mmap(char *region)
+{
+	strcpy(region, "Hello, poftut.com");
+	printf("Contents of region: %s\n", region);
 }
 
 void unmap_page(void *region)
@@ -72,15 +79,64 @@ void unmap_page(void *region)
 		panic("cannot munmap");
 }
 
+void issue_dma(struct ucbdma *ptr)
+{
+	int fd = open("/cbdma/ucopy", O_RDWR);
+
+	printf("[user] ucbdma ptr: %p\n", ptr);
+	write(fd, ptr, sizeof(struct ucbdma *));
+
+	close(fd);
+}
+
+void fill_buffer(char *buffer, char c, int size)
+{
+	memset(buffer, c, size);
+	buffer[size-1] = '\0';
+}
+
+void dump_ucbdma(struct ucbdma *ucbdma)
+{
+	printf("[user] ucbdma: %p\n", ucbdma);
+	printf("[user] \tdesc->xref_size: %d\n", ucbdma->desc.xfer_size);
+	printf("[user] \tdesc->src_addr: %p\n", ucbdma->desc.src_addr);
+	printf("[user] \tdesc->dest_addr: %p\n", ucbdma->desc.dest_addr);
+	printf("[user] \tndesc: %d\n", ucbdma->ndesc);
+	printf("[user] \tstatus: 0x%llx\n", ucbdma->status);
+}
+
 int main()
 {
 	char *region;
+	struct ucbdma *ucbdma;
+	char src[BUFFERSIZE], dst[BUFFERSIZE];
 	
-	region = map_page();
-	
-	strcpy(region, "Hello, poftut.com");
-	printf("Contents of region: %s\n", region);
+	/* setup src and dst buffers */
+	fill_buffer(src, '1', BUFFERSIZE);
+	fill_buffer(dst, '0', BUFFERSIZE);
+	printf("[user] src: %s\n", src);
+	printf("[user] dst: %s\n", dst);
 
+	/* map page for placing ucbdma */
+	region = map_page();
+	// test_mmap(region);
+
+	/* setup ucbdma*/
+	ucbdma = (struct ucbdma *) region;
+	ucbdma->desc.xfer_size = BUFFERSIZE;
+	ucbdma->desc.src_addr  = (uint64_t) src;
+	ucbdma->desc.dest_addr = (uint64_t) dst;
+	ucbdma->ndesc = 1;
+
+	dump_ucbdma(ucbdma);
+	issue_dma(ucbdma);
+	dump_ucbdma(ucbdma);
+
+	printf("[user] channel_status: %llx\n", ucbdma->status);
+	printf("[user] src: %s\n", src);
+	printf("[user] dst: %s\n", dst);
+
+	/* cleanup */
 	unmap_page(region);
 
 	return 0;
